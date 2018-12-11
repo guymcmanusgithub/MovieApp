7f6d2dd47cc8d78bba843ca3d3afc0bcd51c6196
Anhar Athoi <anharzihan@gmail.com>
2018-12-12 09:40:30 +1100

    Converted csv to json

--- server/server.js
+++ server/server.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const app = express();
+const csv=require('csvtojson')
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.get('/', (req, res) => {
-  const movies = [
-    {
-      movieID: 1,
-      title: 'Jurassic Park',  
-    },
-    {
-      movieID: 2,
-      title: 'Jurassic Park 2',  
-    },
-    {
-      movieID: 3,
-      title: 'Jurassic Park 3',  
-    }
-  ];
-
-  res.json(movies);
+  const csvFilePath='movies.csv'
+  csv()
+  .fromFile(csvFilePath)
+  .then((jsonObj)=>{
+    res.send(jsonObj)
+  })
+  // const movies = [
+  //   {
+  //     movieID: 1,
+  //     title: 'Jurassic Park',  
+  //   },
+  //   {
+  //     movieID: 2,
+  //     title: 'Jurassic Park 2',  
+  //   },
+  //   {
+  //     movieID: 3,
+  //     title: 'Jurassic Park 3',  
+  //   }
+  // ];
+ 
+  
 })
 
 const port = 5000;
--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
+import Movies from './Movies.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        {
-          "hello world!"
-        }
-
+        <Movies/>
       </div>
     );
   }
