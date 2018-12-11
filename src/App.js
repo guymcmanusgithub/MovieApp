bd95f6bd69cda6c58b6911ae00c8a0d162de7a9b
Anhar Athoi <anharzihan@gmail.com>
2018-12-11 12:48:48 +1100

    deleted logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,11 +10,6 @@ class App extends Component {
         }
 
       </div>
-
-
-
-
-
     );
   }
 }
